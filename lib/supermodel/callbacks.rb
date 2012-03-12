0d4dd05b603d369eaf018d45974f6980b9caac82
uzzz <uzzable@gmail.com>
2012-03-12 17:59:59 +0300

    added "commit" callback + fixed typo in readme

--- lib/supermodel/callbacks.rb
+++ lib/supermodel/callbacks.rb
@@ -2,13 +2,17 @@ module SuperModel
   module Callbacks
     extend ActiveSupport::Concern
 
+    ALLOWED_CALLBACKS = %w( create save update destroy )
+
     included do
       instance_eval do
         extend ActiveModel::Callbacks
-        define_model_callbacks :create, :save, :update, :destroy
+
+        define_model_callbacks *ALLOWED_CALLBACKS
+        define_model_callbacks :commit # just define; this callback does not work
       end
-      
-      %w( create save update destroy ).each do |method|
+
+      ALLOWED_CALLBACKS.each do |method|
         class_eval(<<-EOS, __FILE__, __LINE__ + 1)
           def #{method}_with_callbacks(*args, &block)
             _run_#{method}_callbacks do
